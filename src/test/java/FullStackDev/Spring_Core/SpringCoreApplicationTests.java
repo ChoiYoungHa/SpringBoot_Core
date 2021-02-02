db9221685225f0b3415b13468b62dd5a74446f7d
YoungHaChoi <askillofgod159@naver.com>
2021-02-02 16:06:42 +0900

    commit

---
+++ src/test/java/FullStackDev/Spring_Core/SpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package FullStackDev.Spring_Core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
