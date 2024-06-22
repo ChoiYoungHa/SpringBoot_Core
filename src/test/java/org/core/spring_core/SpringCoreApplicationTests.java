bf56f2aa45bdd67a4a6c50e298cf9a9be1918fc0
YoungHaChoi <askillofgod159@naver.com>
2024-06-22 20:05:55 +0900

    commit

---
+++ src/test/java/org/core/spring_core/SpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.core.spring_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
